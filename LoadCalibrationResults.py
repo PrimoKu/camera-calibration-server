a5fd68539111e963f108cbe6ad43c62425a14a6c
PrimoKu <primoku.dev@gmail.com>
2024-02-26 12:58:46 -0500

    LoadCalibrationResults

--- LoadCalibrationResults.py
+++ LoadCalibrationResults.py
@@ -1,6 +1,22 @@
 import numpy as np
 
-def load_calibration_results(filename):
+def load_camera_calibration_results(filename):
+    # Load the NPZ file
+    data = np.load(filename)
+
+    # Extract calibration parameters
+    camera_matrix = data['camera_matrix']
+    distortion_coefficients = data['distortion_coefficients']
+    rotation_vectors = data['rotation_vectors']
+    translation_vectors = data['translation_vectors']
+
+    # Print out the results and return them
+    print("Camera Matrix:\n", camera_matrix)
+    print("Distortion Coefficients:\n", distortion_coefficients)
+    print("Rotation Vectors:\n", rotation_vectors)
+    print("Translation Vectors:\n", translation_vectors)
+
+def load_stereo_calibration_results(filename):
     # Load the NPZ file
     data = np.load(filename)
 
@@ -26,5 +42,7 @@ def load_calibration_results(filename):
     print("Essential Matrix:\n", essential_matrix)
     print("Fundamental Matrix:\n", fundamental_matrix)
 
-# Replace 'stereo_calibration_parameters.npz' with the path to your .npz file
-load_calibration_results('stereo_calibration_parameters.npz')
+# Replace 'stereo_calibration_data.npz' with the path to your .npz file
+load_stereo_calibration_results('stereo_calibration_data.npz')
+
+# load_camera_calibration_results('left_calibration_data.npz')
